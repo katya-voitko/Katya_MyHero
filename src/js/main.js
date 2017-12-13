657897aa65eabdc1de056bd698286dec363fe420
Kateryna Voitko <voitko.katerina@gmail.com>
2017-12-13 11:20:50 +0100

    my plugin

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+
+  // IMAGE
+  var myImage = $(".myHero__background").data("image");
+
+  console.log(myImage);
+  $(".myHero__background").css("background-image","url('../img/"+myImage+".jpg')");
+
+ // OPACITY
+
+  var myOpacity = $(".overlay").data("opacity");
+
+  console.log(myOpacity);
+  $(".overlay").css("opacity",myOpacity);
+
+// GREDIENT
+var myGredientColor1 = $(".overlay").data("color1");
+var myGredientColor2 = $(".overlay").data("color2");
+var myGredientDirection = $(".overlay").data("direction");
+console.log(myGredientDirection);
+
+$(".overlay").css("background","linear-gradient("+myGredientDirection+","+myGredientColor1+","+myGredientColor2+")");
+// linear-gradient(to right, #cb356b, #bd3f32);
+
+});
