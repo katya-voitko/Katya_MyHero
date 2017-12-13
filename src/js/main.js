ab79cebf570e26b03fc1405b0b3691f59c3e5066
Kateryna Voitko <voitko.katerina@gmail.com>
2017-12-13 13:16:11 +0100

    last one

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,25 @@ $( document ).ready(function() {
 
   // IMAGE
   var myImage = $(".myHero__background").data("image");
+  var myImageUrl = $(".myHero__background").data("url");
 
-  console.log(myImage);
+
+  console.log(myImageUrl);
   $(".myHero__background").css("background-image","url('../img/"+myImage+"')");
+  $(".myHero__background").css("background-image"," url('"+myImageUrl+"')" );
 
  // OPACITY
 
   var myOpacity = $(".overlay").data("opacity");
 
-  console.log(myOpacity);
+
   $(".overlay").css("opacity",myOpacity);
 
 // GREDIENT
 var myGredientColor1 = $(".overlay").data("color1");
 var myGredientColor2 = $(".overlay").data("color2");
 var myGredientDirection = $(".overlay").data("direction");
-console.log(myGredientDirection);
+
 
 $(".overlay").css("background","linear-gradient("+myGredientDirection+","+myGredientColor1+","+myGredientColor2+")");
 
